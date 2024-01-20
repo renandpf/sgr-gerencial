d1a399552080770258c0c978b4e457e324cf2e5d
Renan Puppo Furtado <renan.puppo@gmail.com>
2024-01-20 19:52:26 -0300

    Add int tests

--- src/test/java/br/com/pupposoft/fiap/sgr/gerencial/produto/adapter/web/ProdutoApiControllerIntTest.java
+++ src/test/java/br/com/pupposoft/fiap/sgr/gerencial/produto/adapter/web/ProdutoApiControllerIntTest.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -27,14 +27,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import br.com.pupposoft.fiap.sgr.gerencial.cliente.adapter.web.json.ClienteJson;
-import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.controller.ClienteController;
-import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.dto.ClienteDto;
-import br.com.pupposoft.fiap.sgr.gerencial.cliente.core.exception.ClienteNaoEncontradoException;
+import br.com.pupposoft.fiap.sgr.gerencial.produto.adapter.web.json.ProdutoJson;
 import br.com.pupposoft.fiap.sgr.gerencial.produto.core.controller.ProdutoController;
 import br.com.pupposoft.fiap.sgr.gerencial.produto.core.domain.Categoria;
 import br.com.pupposoft.fiap.sgr.gerencial.produto.core.dto.ProdutoDto;
-import br.com.pupposoft.fiap.test.databuilder.DataBuilderBase;
 
 @WebMvcTest(ProdutoApiController.class)
 class ProdutoApiControllerIntTest {
@@ -90,6 +86,87 @@ class ProdutoApiControllerIntTest {
 		verify(produtoController).obterById(param);
 	}
 	
-	//TODO: continuar
+	@Test
+	void shouldSucessOnCriar() throws Exception {
+		final Long newProductId = 15L;
+		
+		ProdutoJson requestBody = ProdutoJson.builder()
+				.id(1L)
+				.nome("cliente nome")
+				.descricao("any descrição")
+				.valor(new BigDecimal("3.55"))
+				.categoria("LANCHE")
+				.build();
+		doReturn(newProductId).when(produtoController).criar(any(ProdutoDto.class));
+		
+		this.mockMvc.perform(
+				post("/sgr/gerencial/produtos")
+				.content(objectMapper.writeValueAsString(requestBody))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(content().string(newProductId + ""));
+		
+		ArgumentCaptor<ProdutoDto> produtoDtoAC = ArgumentCaptor.forClass(ProdutoDto.class);
+		
+		verify(produtoController).criar(produtoDtoAC.capture());
+		
+		ProdutoDto produtoSentTOCreate = produtoDtoAC.getValue();
+		
+		assertNull(produtoSentTOCreate.getId());
+		assertEquals(requestBody.getNome(), produtoSentTOCreate.getNome());
+		assertEquals(requestBody.getDescricao(), produtoSentTOCreate.getDescricao());
+		assertEquals(requestBody.getValor(), produtoSentTOCreate.getValor());
+		assertEquals(0L, produtoSentTOCreate.getCategoriaId());
+		
+	}
+	
+	@Test
+	void shouldSucessOnAlterar() throws Exception {
+		final Long newProductId = 15L;
+		
+		ProdutoJson requestBody = ProdutoJson.builder()
+				.id(1L)
+				.nome("cliente nome")
+				.descricao("any descrição")
+				.valor(new BigDecimal("3.55"))
+				.categoria("LANCHE")
+				.build();
+		
+		this.mockMvc.perform(
+				put("/sgr/gerencial/produtos/" + newProductId)
+				.content(objectMapper.writeValueAsString(requestBody))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().string(""));
+		
+		ArgumentCaptor<ProdutoDto> produtoDtoAC = ArgumentCaptor.forClass(ProdutoDto.class);
+		
+		verify(produtoController).alterar(produtoDtoAC.capture());
+		
+		ProdutoDto produtoSentTOCreate = produtoDtoAC.getValue();
+		
+		assertEquals(newProductId, produtoSentTOCreate.getId());
+		assertEquals(requestBody.getNome(), produtoSentTOCreate.getNome());
+		assertEquals(requestBody.getDescricao(), produtoSentTOCreate.getDescricao());
+		assertEquals(requestBody.getValor(), produtoSentTOCreate.getValor());
+		assertEquals(0L, produtoSentTOCreate.getCategoriaId());
+	}
+	
+	@Test
+	void shouldSucessOnExcluir() throws Exception {
+		final Long deleteProductId = 15L;
+		
+		this.mockMvc.perform(
+				delete("/sgr/gerencial/produtos/" + deleteProductId))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().string(""));
+		
+		verify(produtoController).excluir(deleteProductId);
+	}
+	
+	
 	
 }
